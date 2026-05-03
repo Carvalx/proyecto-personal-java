97d72c06d6bc0531daba46dfb3382e4793a6d108
Carvalx <carvadel@hotmail.com>
2026-05-03 20:07:56 +0200

    bucles de for y while

---
+++ Bucles.java
@@ -0,0 +1,19 @@
+public class Bucles {
+    public static void main(String[] args) {
+        //For
+        for (int i=1; i<=10; i++) {
+            int par = i % 2;
+            if (par == 0){
+                System.out.println(i);
+            } 
+        }
+
+        //While
+        int j = 10;
+        while (j >= 1) {
+            System.out.println(j);
+            j--;
+        }
+        System.out.println("Despegue!!!");
+    }
+}
