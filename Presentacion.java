83d3b0ffb251be35652d843c065387b0d3545fb3
Carvalx <carvadel@hotmail.com>
2026-05-03 19:19:29 +0200

    aprendizaje de sintaxis java en variables y cadenas

---
+++ Presentacion.java
@@ -0,0 +1,7 @@
+public class Presentacion {
+    public static void main(String[] args) {
+        System.out.println("Carlos Macero");
+        System.out.println("Guayaquil");
+        System.out.println("Java, Python, PHP");
+    }
+}
---
+++ Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+        String name = "Carlos";
+        int edad = 28;
+        double altura = 1.7;
+        boolean trabajando = false;
+        char inicialApellido = 'M';
+
+        System.out.println("Mi nombre es " + name + ", tengo " + edad + 
+        ", mi altura es " + altura + ". Tengo: " + trabajando + 
+        " y mi apellido empieza con " + inicialApellido);
+    }
+}
